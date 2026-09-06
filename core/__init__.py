e474963d349911b1ca004bf83efacd617e15b69e
Hernan Cussit <23178440+hernancussit@users.noreply.github.com>
2026-09-06 20:35:40 -0300

    fix: expose plugin manifest and core at repository root for 1-click dHtools cloning

---
+++ core/__init__.py
@@ -0,0 +1,9 @@
+"""
+Core modules for Remote Assist plugin (dHtools)
+"""
+from .telegram_bot import TelegramBot
+from .channels import NotificationDispatcher
+from .cloud_uploader import CloudUploader
+from .access_manager import AccessManager
+
+__all__ = ["TelegramBot", "NotificationDispatcher", "CloudUploader", "AccessManager"]
